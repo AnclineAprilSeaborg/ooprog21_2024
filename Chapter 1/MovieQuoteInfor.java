7cf45d4a06605bbf4e77b95d0f723049da672de3
kymir <155278226+Ancline@users.noreply.github.com>
2024-08-14 17:42:53 +0800

    Update MovieQuoteInfor.java

--- Chapter 1/MovieQuoteInfor.java
+++ Chapter 1/MovieQuoteInfor.java
@@ -1,1 +1,8 @@
-
+public class MovieQuoteInfor {
+   public static void main(String[] args){
+   
+      System.out.print("I have lived for a thousand years. I may live a thousand more... or I may die tomorrow. But I no longer fear death ... for I have known it once already.");
+      System.out.print("\n -Selene, Underworld: Blood Wars (2016)");
+   
+   }
+}
