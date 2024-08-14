fda7c24043cb2d2d4706a7a0769c5786334991a4
kymir <155278226+Ancline@users.noreply.github.com>
2024-08-14 17:53:58 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -1,1 +1,15 @@
-
+public class TableAndChairs{
+   public static void main (String[] args){
+      
+      System.out.print("*                                    * " + 
+                       "\n*                                    *" +
+                       "\n*                                    *" +
+                       "\n*                                    *" +
+                       "\n*********    ***********    **********" +
+                       "\n*       *    *         *    *        *" +
+                       "\n*       *    *         *    *        *" +
+                       "\n*       *    *         *    *        *" + 
+      "" );
+      
+   }
+}
