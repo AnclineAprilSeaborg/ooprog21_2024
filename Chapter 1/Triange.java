7f4d0e0581dbb6c8a42680b87ea752870cf3c49b
kymir <155278226+Ancline@users.noreply.github.com>
2024-08-14 17:54:55 +0800

    Create Triange.java

---
+++ Chapter 1/Triange.java
@@ -0,0 +1,12 @@
+public class Triange{
+   public static void main (String[] args){
+   
+       System.out.print("              |              "+
+                      "\n             |||            " +
+                      "\n            |||||          " +
+                      "\n           |||||||          " +
+                      "\n          |||||||||         " +
+      
+      "");
+   }
+}
