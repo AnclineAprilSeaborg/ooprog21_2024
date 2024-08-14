d2a46a3233b756a7af61a73d75723ebbca8326f3
Ancline <155278226+Ancline@users.noreply.github.com>
2024-08-14 18:22:32 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -1,14 +1,14 @@
 public class TableAndChairs{
    public static void main (String[] args){
       
-      System.out.print("*                                    * " + 
-                       "\n*                                    *" +
-                       "\n*                                    *" +
-                       "\n*                                    *" +
-                       "\n*********    ***********    **********" +
-                       "\n*       *    *         *    *        *" +
-                       "\n*       *    *         *    *        *" +
-                       "\n*       *    *         *    *        *" + 
+      System.out.print("x                                    x " + 
+                       "\nx                                    x" +
+                       "\nx                                    x" +
+                       "\nx                                    x" +
+                       "\nxxxxxxxxx    xxxxxxxxxxx    xxxxxxxxxx" +
+                       "\nx       x    x         x    x        x" +
+                       "\nx       x    x         x    x        x" +
+                       "\nx       x    x         x    x        x" + 
       "" );
       
    }
