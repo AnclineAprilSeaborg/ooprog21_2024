24dd5f3e992a0fa4f77bfd93c13e01853dd74663
AnclineAprilSeaborg <155278226+AnclineAprilSeaborg@users.noreply.github.com>
2024-09-20 18:28:36 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,17 @@
+public class FirstAddressExcercise{
+   public static void main(String[] args){
+   
+   invokeAddress();
+   
+   }
+   
+   public static void invokeAddress(){
+   
+   System.out.println("Samsung" +
+                      "\n129 Samsung-ro" +
+                      "\nYeongtong-gu, Suwon" + 
+                      "\nGyeonggi-do, South Korea" +
+                      "\nCompany: Samsung ");
+   
+   }
+}
