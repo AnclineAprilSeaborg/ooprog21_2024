e8edf3c10df0b5cb868e662dce33bddaf4232d4f
AnclineAprilSeaborg <155278226+AnclineAprilSeaborg@users.noreply.github.com>
2024-10-02 15:39:48 +0800

    Create MathTest.java

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,14 @@
+public class MathTest {
+    public static void main(String[] args) {
+        
+        System.out.println("SQRT: " + Math.sqrt(26)); 
+        System.out.println("SIN: " + Math.sin(32)); 
+        System.out.println("COS: " + Math.cos(24)); 
+        System.out.println("FLOOR: " + Math.floor(22.94)); 
+        System.out.println("CEIL: " + Math.ceil(41.7235));    
+        System.out.println("ROUND: " + Math.round(44.6)); 
+        System.out.println("MAX: " + Math.max(12, 87)); 
+        System.out.println("MIN: " + Math.min(31, 11));
+        System.out.println("RANDOM: " + Math.random() * 100);
+    }
+}
